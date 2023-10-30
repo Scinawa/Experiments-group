92d4109dba5e403c24f0fa0cb24e34159120fc48
Scinawa <2940017+Scinawa@users.noreply.github.com>
2023-10-30 19:08:24 +0800

    Refactored experiments, added atlasn=7

---
+++ helper_common_files.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import scipy
+import scipy.io
+import pickle
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import time
+from sklearn.decomposition  import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+#from spectrum_utils import *
+import numpy as np
+import warnings
+
+
+######## Machine learning stuff
+
+
+def PCA_everything(dataset, ncomponents):
+    pca = PCA(n_components= ncomponents)
+    return pca.fit_transform(dataset)
+
+
+def clean_it(skew_spectra):
+    return  np.round(skew_spectra, decimals=6)
+
+def make_example(kcorrelation, stop_at_length=None):
+
+    if stop_at_length:
+        indices = list(range(len(kcorrelation[:stop_at_length])))    # limit here the size of the final tree, i.e. the number of grpahs we consider
+    else:
+        indices = list(range(len(kcorrelation)))
+
+    newlist =  []
+
+    for i in indices:
+        newlist.append(kcorrelation[i])
+
+    random.Random(4).shuffle(indices)
+
+    for i in indices:
+        newlist.append(kcorrelation[i])
+
+    print("len of newlist", len(newlist))
+    return np.array(newlist)
+
+
+
+def create_T_table_2(k_correlations):
+    """
+    This function creates the T table for the k_correlations
+    Very slow function, use only for small datasets
+    """
+    T_sets = {}
+
+    for kcorre_name in kcorre_names:
+        T_sets[kcorre_name]=[]
+        indices = set(range(len(k_correlations[kcorre_name])))
+        #print("indices", indices)
+
+        dista = sk.metrics.pairwise_distances(k_correlations[kcorre_name])
+
+        while True:
+            row = random.choice(list(indices))
+
+
+            #agiowhere = np.where(
+            #    np.isclose(dista[row], np.zeros(len(dista[row])))
+            #    )
+            #print(list(agiowhere[0]))
+            group = set(np.where(
+                np.isclose(dista[row], np.zeros(len(dista[row])))
+                )[0]           )
+            print("group:", group, end=' ')
+            T_sets[kcorre_name].append(group)
+            indices = indices - group
+
+            if len(indices)==0:
+                print("Finished clustering!")
+                break
+            else:
+                print(len(indices), end=', ')
+
+    return T_sets   
+
+
+def create_T_table(k_correlations, kcorre_names):
+    """
+    Same thing as before, but super fast! 
+
+    """
+    T_tmp = {}
+    T_sets = {}
+
+    for kcorre_name in kcorre_names:
+
+        T_tmp[kcorre_name]={}
+
+        # Iterate through the skew_spectra and populate the dictionary
+        for index, vector in enumerate(k_correlations[kcorre_name]):
+            #print(index)
+            # Convert the vector to a tuple to use it as a dictionary key :) 
+            vector_tuple = tuple(vector)
+
+            if vector_tuple in T_tmp[kcorre_name]:
+                # If the vector representation exists in the dictionary, append the index
+                T_tmp[kcorre_name][vector_tuple].append(index)
+            else:
+                # Oth.. create a new entry with the index
+                T_tmp[kcorre_name][vector_tuple] = [index]
+
+        # Finally, convert the dictionary values to sets
+        T_sets[kcorre_name] = [set(indices) for indices in T_tmp[kcorre_name].values()]
+    return T_sets
+
+
+
+def build_networkx_graph(T, maxk):
+    G = nx.DiGraph()
+    G.add_node((0,0))
+
+    breakout = False
+
+    for k in range(maxk, 1, -1):
+        #print(k)
+        for subsetz in T['1orbit-{}-corre-dict'.format(k+1)]:
+            for setz in T['1orbit-{}-corre-dict'.format(k)]:
+                #print("k:", k, "subset", subsetz, "set:", setz, "k:", k)
+
+
+                for node in subsetz:                
+                    if {node}.issubset(setz):
+                        G.add_edge((k, ) + tuple(setz), (k+1,) + tuple(subsetz) )
+                    else:
+                        pass 
+                        #G.add_node( (k+1,) + tuple(subsetz)  )
+                        #print("anomaly detected")
+            
+
+    for setz in T['1orbit-2-corre-dict']:
+        G.add_edge((0,0), (2,) + tuple(setz))
+    return G
+
+
+
+def graph_subtraction_nodes(G, Gbfs):
+    Gclone = G.copy()
+    for node in Gbfs:
+        Gclone.remove_node(node)
+
+    return Gclone
+
+
+def graph_subtraction_edges(G, Gbfs):
+    Gclone_edges = G.copy()
+    for edge in Gbfs.edges():
+        Gclone_edges.remove_edge(*edge)
+    return Gclone_edges
+
+
+def count_bifurcations(grafetto, kcorre_names):
+    histogram = {kcorre_name : 0 for kcorre_name in kcorre_names}
+    histogram['1orbit-0-corre-dict'] = 0
+    hits = []
+    lottery_tickets = []
+    for node in grafetto.nodes():
+        if grafetto.out_degree(node)>1:
+            histogram['1orbit-{}-corre-dict'.format(node[0])] +=  grafetto.out_degree(node)  # is this right? consider {1,2,3,4} -> {1,2,3}, {4} or {1,2,3,4} -> {1,2}, {3}, 
+            hits.append(node)
+            if node[0] > 2:      # we can distinguish graphs in this node in the NEXT k of correlation
+                lottery_tickets.append(node)
+    return histogram, hits, lottery_tickets
+
+def count_collisions(grafetto, kcorre_names):
+    histogram = {kcorre_name : 0 for kcorre_name in kcorre_names}
+    histogram['1orbit-0-corre-dict'] = 0
+    hits = []
+    for node in grafetto.nodes():
+        #print(grafetto.in_degree(node))
+        if grafetto.in_degree(node)>1:
+            histogram['1orbit-{}-corre-dict'.format(node[0])] +=  grafetto.in_degree(node)  # is this right? consider {1,2,3,4} -> {1,2,3}, {4} or {1,2,3,4} -> {1,2}, {3}, 
+            hits.append(node)
+    return histogram, hits
+
+####### POSITION OF NODES  
+    # def position_of_nodes(G):
+#     #pos = nx.spring_layout(G, seed=10)
+
+#     total_nodes = list(G.nodes())
+
+
+#     #pprint.pprint(total_nodes)
+
+#     k = 10
+#     for n in total_nodes:
+#         cc = nx.bfs_tree(G, n)
+ 
+#         k = k + 1
+#         for node in cc:
+#             #print(node)
+#             pos[node] = np.array([k, node[0]])
+#             total_nodes.remove(node)
+
+#     # handle remaining nodes
+#     pass
+#     #print("\n\n")
+#     #pprint.pprint(pos)
+
+#     return pos
