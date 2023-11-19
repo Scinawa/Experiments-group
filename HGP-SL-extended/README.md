4e3bc541a3df9e57b33a8c7f8b06972778c15422
Scinawa <2940017+Scinawa@users.noreply.github.com>
2023-11-19 09:51:45 +0100

    Add gitignore, statistics, scriptozzo.

---
+++ HGP-SL-extended/README.md
@@ -0,0 +1,12 @@
+On my machine
+tar -zcvf PROTEINS.tar.gz PROTEINS/
+scp PROTEINS.tar.gz nus8:/home/svu/cqtales/Experiments-Groups/HGP-SL-extended/
+
+On the HPC machine:
+tar zcvf TUProteins_skew.tar.gz TUProteins/
+
+
+On my machine again
+scp nus8:/home/svu/cqtales/Experiments-Groups/HGP-SL-extended/TUProteins_skew.tar.gz .
+
+n
---
+++ HGP-SL-extended/compute_statistics.py
@@ -0,0 +1,39 @@
+import statistics
+
+
+import subprocess
+
+
+
+for filename in ["corre3.txt", "corre2.txt", "original.txt"]:
+
+    result = subprocess.run("cat {} | grep Test ".format(filename), shell=True, capture_output=True, text=True)
+
+    # Get the output
+    text = result.stdout
+
+
+    # text = """
+    # Test set results, loss = 0.565025, accuracy = 0.702128
+    # Test set results, loss = 0.584491, accuracy = 0.723404
+    # Test set results, loss = 0.587194, accuracy = 0.702128
+    # Test set results, loss = 0.516932, accuracy = 0.734043
+    # Test set results, loss = 0.625465, accuracy = 0.648936
+    # Test set results, loss = 0.468312, accuracy = 0.819149
+    # """
+
+    lines = text.strip().split('\n')
+    try: 
+        accuracies = [float(line.split()[-1]) for line in lines]
+
+        average = statistics.mean(accuracies)
+        variance = statistics.variance(accuracies)
+        print('File: {}'.format(filename))
+        print('Average: {:.6f}'.format(average))
+        print('Variance: {:.6f}'.format(variance))
+        print('Max:{:.6f}'.format(max(accuracies)) )
+
+        from time import sleep
+        sleep(1)
+    except Exception as e:
+        print(e)
