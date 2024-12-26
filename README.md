36c475005ef70d470ee79fb879639e669e2aede5
Scinawa <2940017+Scinawa@users.noreply.github.com>
2024-12-26 08:25:44 +0800

    Refactoring dataset-combinatorial-graphs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Experiments-Groups
+# Experiments on multi-orbit reduced skew spectrum for group-theoretical ML
 
 
 We have the following datasets:
@@ -25,3 +25,115 @@ We have the following experimeents with Neural Networks:
 - http://users.cecs.anu.edu.au/%7Ebdm/data/graphs.html
 
 pip install torch_geometric==1.7.2
+
+
+# Experiments 
+
+
+## 1 EXP-new-QM7-regression.ipynb
+This is the same content as in the extended abstract.
+
+---
+
+## 2 EXP-on-graphs-generate-shattering-plots.ipynb
+All these experiments can be repeated for **any** group of graphs. In the file `Exp-on-graphs.ipynb`, you will find several interesting experiments.
+
+*Note*: You can perform these experiments on any imaginable graph dataset. For example:
+- All undirected graphs with 2, 3, 4, 5, 6, 7 nodes  
+- Euler graphs (for various *n*)  
+- Chordal graphs (for various *n*)  
+- Planar graphs (for various *n*)  
+- etc.
+
+### Quesiton: Do we gain more distinguishing power by increasing *k* for the k-reduced-skew-spectrum?
+Here we show that there are graphs that become distinguishable only when we increase *k*. We call these "lottery tickets." For example, in the case of `atlas7`, we found 2 lottery tickets, i.e., nodes that split at depth `k>3`:
+
+```
+{'1orbit-0-corre-dict': 712,
+ '1orbit-2-corre-dict': 518,
+ '1orbit-3-corre-dict': 4,
+ '1orbit-4-corre-dict': 0,
+ '1orbit-5-corre-dict': 0,
+ '1orbit-6-corre-dict': 0,
+ '1orbit-7-corre-dict': 0,
+ '1orbit-8-corre-dict': 0}
+
+The number of nodes is 4868
+The number of graphs is 1044
+
+The lottery tickets are:
+[(3, 417, 419), (3, 555, 556)]
+```
+
+![image](./lottery-tickets.png)
+
+**TODO**: We realized that for `atlas-undirected-7nodes` (which has 1044 non-isomorphic graphs), a possible value for `[726, 1042, 1043, 1041, 1029]` works. This assumes the skew-spectra are generated with enough precision, and that the two algorithms (`skew2table` and `table2tree`) used behind the data structures produce correct numbers.
+
+Below is the figure for `atlas7` from the experiments we did together:  
+![image](./grafico-fatto-assieme.png)
+
+---
+
+### Quesiton: How does it compare the morsksp across different datasets
+We can take different datasets, generate a plot (x-axis = skew-spectrum, y-axis = number of different morsKSP), and see where we perform “better.” As we found, for some graphs, morsKSP can distinguish almost everything, while for others it loses a lot of information, no matter what *k* is.
+
+For example:
+![image](./highly-irregular.png)
+![image](./eul8-c.png)
+
+---
+
+## 3 Morksp as additional feature in neural networks (HGP-SL-extended)
+This folder contains the code for a neural network that can be trained on different datasets. The `bash` scripts compute averages, variances, and maximum values for the various datasets (for instance, `experiments-NC1.sh`).
+
+The code is split into `main.py` and `models.py` for the original parts. Our modifications are in `extended-models.py` and `extended-main.py`. 
+
+This code can recompute skew-spectra on the fly, but it can also dump or reuse skew-spectra from HPC machines. The idea of the `Model` class is to perform standard graph convolution in ML while also adding skew-spectrum features. We observed that this approach performs better if we pick the best models (i.e., those that generalize best), which matches the approach in the paper we’re comparing against—they report models that generalize better, rather than average performance.
+
+On a laptop, we can process graphs up to 25 nodes; on HPC machines, we reached up to 50–51 nodes.
+
+![image](./HGP-SL-extended/accuracies-PROTEINS.png)  
+![image](./HGP-SL-extended/accuracies-second-PROTEINS.png)  
+![image](./HGP-SL-extended/accuracies-third-PROTEINS.png)
+
+### Split and validation test set
+```python
+random_split comes from torch.utils.data import random_split
+
+num_training = int(len(dataset) * 0.8)
+num_val = int(len(dataset) * 0.1)
+num_test = len(dataset) - (num_training + num_val)
+
+training_set, validation_set, test_set = random_split(dataset, [num_training, num_val, num_test])
+```
+
+### Note on the PROTEINS dataset
+For PROTEINS, we used `n=59` nodes.
+
+---
+
+# ALGORITHM
+**Node name definition**: `(k, [id, id, …])`  
+- `k`: correlation level  
+- `id`: IDs of graphs with that skew-thing
+
+For each node at depth `k=3` or `k=4` (the ones we care about with 1043 nodes), we get the node name.
+
+```python
+tmp_id_interesting_graph_ = 0
+tmp_id_interesting_graph = 0
+for g in G_dfs:
+    if g[0] == 3:
+        if len(g.name) > 2:
+            print(g.name)
+            tmp_id_interesting_graph = g.name[1]
+            tmp_id_interesting_graph_ = g.name[2]
+
+print(raw_dataset[0][tmp_id_interesting_graph])  
+```
+
+# WIP
+- Experiments with proteins (MO). Download them locally from the NUS server where they are being computed.
+- Make ENZYMES work: debug the two `forward()` functions to ensure the same tensor shape and batch size.
+- Test that the format for the `morsksp` export is correct locally and can be read properly.
+- Experiment with all graphs downloaded from the website.
--- dataset-combinatorial-graphs/generate-skew-from-g6.py
+++ dataset-combinatorial-graphs/generate-skew-from-g6.py
@@ -1,27 +1,16 @@
 import sys
 import numpy as np
-
 from tqdm import tqdm
-import sys
-sys.path.append('..')
-#from model import Net
-#from utils.config import process_config, get_args
-#from utils.basis_transform import basis_transform
-#from utils.config import get_config_from_json
-import random
-import time
+
 import pickle
 
-# from IPython import embed; embed()
-
-
+sys.path.append('..')
 sys.path.append('/Users/scinawa/workspace/grouptheoretical/multi-orbit-bispectrum-main/')
 from spectrum_utils import * 
 from utils import *
 
 import networkx as nx
-
-
+import argparse
 
 
 
@@ -31,6 +20,72 @@ def graph_from_fileg6(filepath):
     Lines = file1.readlines()
     for line in Lines:
         yield nx.from_graph6_bytes(line.strip().encode("utf-8"))
+
+
+
+
+def generate_from_g6(filepath, multi_orbit, k_correlation):
+    skew_spectrums = {}
+    graphs = []
+
+    if multi_orbit:
+        for k in range(2,8):
+            skew_spectrums["2orbit-{}-corre-dict".format(k)]=[]
+    else:
+        for k in range(2,8):
+            skew_spectrums["1orbit-{}-corre-dict".format(k)]=[]
+
+
+    for nxgraph in tqdm(graph_from_fileg6(filepath), desc="Generating skew spectrums"):
+
+        graph = nx.to_numpy_array(nxgraph)
+        graphs.append(nxgraph)
+
+        # print("Creating correlation ")
+        for k in range(2, k_correlation):
+            #print("{} ".format(k), end="")
+
+            if multi_orbit:
+                func_ = create_func_on_group_from_matrix_2orbits(np.array(graph))
+                skew_spectrums["2orbit-{}-corre-dict".format(k)].append(
+                    reduced_k_correlation(func_, k=k, method="extremedyn", vector=True))
+
+            else:
+                func_ = create_func_on_group_from_matrix_1orbit(graph)
+                skew_spectrums["1orbit-{}-corre-dict".format(k)].append(
+                    reduced_k_correlation(func_, k=k, method="extremedyn", vector=True))
+
+
+
+    ############ USE WITH CARE!!!!!! IT WILL OVERWRITE THE PERVIOUSLY COMPUTED FEATURES FILE
+    if multi_orbit:
+        with open("{}-mo-rsksp.pickle".format(filepath), 'wb') as handle:
+            pickle.dump((graphs, skew_spectrums), handle, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        with open("{}-so-rsksp.pickle".format(filepath), 'wb') as handle:
+            pickle.dump((graphs, skew_spectrums), handle, protocol=pickle.HIGHEST_PROTOCOL)
+     
+     
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate skew spectra from a graph6 file.")
+    parser.add_argument("filepath", type=str, help="Path to the graph6 file.")
+    parser.add_argument("--multi_orbit", action="store_true", help="Use multi-orbit correlation (default: False).")
+    parser.add_argument("--k-correlation", type=int, default=8, help="Value of k for k-correlation (default: 2).")
+
+    args = parser.parse_args()
+
+    print("Generating skew spectra from {}".format(args.filepath))
+    file1 = open(args.filepath, 'r')
+    Lines = file1.readlines()
+    print("There are {} graphs".format(len(Lines)))
+
+
+
+    generate_from_g6(args.filepath, multi_orbit=args.multi_orbit, k_correlation=args.k_correlation)
+    print("Done.")
+
+
+
 
 
 
@@ -108,65 +163,3 @@ def graph_from_fileg6(filepath):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-def generate_from_g6(filepath, multi_orbit=True):
-    skew_spectrums = {}
-    graphs = []
-
-    if multi_orbit:
-        for k in range(2,8):
-            skew_spectrums["2orbit-{}-corre-dict".format(k)]=[]
-    else:
-        for k in range(2,8):
-            skew_spectrums["1orbit-{}-corre-dict".format(k)]=[]
-
-
-    for nxgraph in tqdm(graph_from_fileg6(filepath), desc="Generating skew spectrums of all graphs"):
-
-
-        graph = nx.to_numpy_array(nxgraph)
-        graphs.append(nxgraph)
-
-        for k in range(2,8):
-            print("Creating {}-th correlation".format(k))
-
-            if multi_orbit:
-                func_ = create_func_on_group_from_matrix_2orbits(np.array(graph))
-                skew_spectrums["2orbit-{}-corre-dict".format(k)].append(
-                    reduced_k_correlation(func_, k=k, method="extremedyn", vector=True))
-
-            else:
-                func_ = create_func_on_group_from_matrix_1orbit(graph)
-                skew_spectrums["1orbit-{}-corre-dict".format(k)].append(
-                    reduced_k_correlation(func_, k=k, method="extremedyn", vector=True))
-
-
-
-    ############ USE WITH CARE!!!!!! IT WILL OVERWRITE THE PERVIOUSLY COMPUTED FEATURES FILE
-    if multi_orbit:
-        with open("{}-mo-rsksp.pickle".format(filepath), 'wb') as handle:
-            pickle.dump((graphs, skew_spectrums), handle, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        with open("{}-so-rsksp.pickle".format(filepath), 'wb') as handle:
-            pickle.dump((graphs, skew_spectrums), handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-
-
-if __name__ == "__main__":
-    print("Generating skew spectra from {}".format(sys.argv[1]) )
-    
-    
-    #generate_random_skew()
-    generate_from_g6(sys.argv[1], multi_orbit=True)
-    generate_from_g6(sys.argv[1], multi_orbit=False)
