2d0bc90699b40a19101cfae45e8841d9f7fe996d
Scinawa <2940017+Scinawa@users.noreply.github.com>
2024-12-26 13:54:43 +0800

    Make all dataset-* folder into similar format, add datasets to HGP folder

--- generate_all_graphs/doit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-#PBS -l mem=10GB
-
-for i in para2
-do
-   python3 /home/svu/cqtales/generate_ALL_graphs/generate_graphs.py 9 $i
-done
--- generate_all_graphs/generate_graphs6.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-
-python3 /home/svu/cqtales/generate_ALL_graphs/generate_graphs.py 6 para4
-
